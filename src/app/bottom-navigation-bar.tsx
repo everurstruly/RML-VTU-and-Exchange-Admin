b2f22094e8c2b6d9eb2d710e5dbf7f99c36362b4
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-11 19:00:53 -0700

    feat: re-select bottom navigation actions

--- src/app/bottom-navigation-bar.tsx
+++ src/app/bottom-navigation-bar.tsx
@@ -1,48 +1,61 @@
 import * as React from "react";
+import { NavLink } from "react-router-dom";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Paper } from "@mui/material";
-import { Forum, Home, LocalMall } from "@mui/icons-material";
+import {
+  AccountCircle,
+  AccountCircleSharp,
+  Forum,
+  Home,
+  LocalMall,
+  ShoppingBasket,
+} from "@mui/icons-material";
 
 export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
 
   return (
     <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 100 }}
       elevation={3}
     >
       <BottomNavigation
         showLabels
-        value={value}
-        onChange={(evt, newValue) => {
-          setValue(newValue);
-        }}
-        className="!h-[68px]"
+        // value={value}
+        // onChange={(evt, newValue) => {
+        //   setValue(newValue);
+        // }}
+        className="!h-[74px]"
       >
         <BottomNavigationAction
-          className="gap-y-1"
-          href="/home"
+          // LinkComponent={NavLink}
+          className="gap-y-1.5 !justify-end !pb-2"
+          href="/dashboard"
           label="Dashboard"
-          icon={<Home />}
+          icon={<Home className="!size-7" />}
         />
         <BottomNavigationAction
-          className="gap-y-1"
+          // LinkComponent={NavLink}
+          className="gap-y-1.5 !justify-end !pb-2"
           href="/orders"
           label="Orders"
-          icon={<LocalMall />}
+          icon={<LocalMall className="!size-7" />}
         />
         <BottomNavigationAction
-          className="gap-y-1"
+          // LinkComponent={NavLink}
+          className="gap-y-1.5 !justify-end !pb-2"
           href="/messages"
           label="Messages"
-          icon={<Forum />}
+          icon={<Forum className="!size-7" />}
         />
         <BottomNavigationAction
-          className="gap-y-1"
-          label="More"
-          icon={<MenuIcon />}
+          // LinkComponent={NavLink}
+          className="gap-y-1.5 !justify-end !pb-2"
+          href="/me"
+          label="You"
+          icon={<AccountCircle className="!size-7" />}
         />
       </BottomNavigation>
     </Paper>
