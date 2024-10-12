94b32163e6f335ab0f2c8dfb1f81bfdc8866fd89
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-12 03:24:30 -0700

    feat: update ui aesthetics

--- src/app/top-navigation-bar.tsx
+++ src/app/top-navigation-bar.tsx
@@ -78,7 +78,7 @@ export default function TopNavigationBar(props: Props) {
 
           <Button
             color="primary"
-            variant="outlined"
+            variant="contained"
             className="!ms-auto !capitalize !font-medium"
             size="small"
           >
