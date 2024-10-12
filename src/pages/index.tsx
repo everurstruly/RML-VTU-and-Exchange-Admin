b2550202929de38f198276b709519f944ca14a56
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-12 03:18:20 -0700

    feat: beautify and minify page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,19 @@
 import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
-import viteLogo from "/vite.svg";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="w-full h-full pb-40">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen pb-40 grid place-content-center">
+      <div className="p-4 bg-white mx-auto">
+        <h4 className="text-lg font-medium text-center mb-4">Count: {count}</h4>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="btn btn-primary rounded-md py-2 px-4"
+        >
+          Click me to count
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
