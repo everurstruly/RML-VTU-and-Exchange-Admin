45c9099d2731197d873da91ef77df293234ade0a
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-13 15:10:15 -0700

    feat: define orders details routes components

---
+++ src/pages/orders/crypto.[id].tsx
@@ -0,0 +1,44 @@
+export default function CryptoOrdersItemPage() {
+  return <div className="min-h-screen pt-8">Crypto Orders Details Page</div>;
+
+  // Continue or Back+Next
+  // I want to reject the order
+}
+
+/*
+appealing, reviewing, processing, rejected
+*/
+
+
+
+
+/*
+
+Review the order -> Accept, Reject 
+
+on ACCEPT
+1. confirm order with user receipt and all
+2. calc/identify amount to send to the user 
+    AND identify the account details (with copy functions) to send to
+3. complete Order - make payments
+    (Optional) upload payment receipt
+    Complete Order
+
+
+on REJECTED
+1. Select from a list of reasons (rejected)
+2. If (refuse appeal) -> Select from a list of reasons
+2. Add optional additionl message
+3. Complete Order
+
+
+*/
+
+
+
+
+/*
+
+users can appeal rejected orders (the higher the level, the higher the appeal chances)
+
+*/
---
+++ src/pages/orders/giftcard.[id].tsx
@@ -0,0 +1,3 @@
+export default function GiftCardOrdersItemPage() {
+  return <div>Gift Card Orders Details Page</div>;
+}
