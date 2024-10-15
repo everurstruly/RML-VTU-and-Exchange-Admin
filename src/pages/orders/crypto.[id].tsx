89145a7a5ea2d6fcafb22c253f7e6321f3dcae62
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-14 17:23:41 -0700

    feat: add dynamic stepper and order processing components draft

--- src/pages/orders/crypto.[id].tsx
+++ src/pages/orders/crypto.[id].tsx
@@ -1,5 +1,142 @@
+import { Button, Stack } from "@mui/material";
+import DynamicStepper, {
+  StepItemHandlers,
+  StepperState,
+} from "../../app/dynamic-stepper";
+import GiftCardOrderReviewCard from "../../app/giftcard-order-review-card";
+import PageTitle from "../../app/page-title";
+
+const initGeneralSteps = [
+  {
+    label: "Review Order",
+    renderBody: (
+      stepper: StepperState,
+      { handleNext, handleAccept, handleReject }: StepItemHandlers
+    ) => {
+      return (
+        <>
+          <GiftCardOrderReviewCard
+            coverUri="https://via.placeholder.com/150"
+            userId="32UD457"
+            name="Steam"
+            region="USA"
+            amount={29.99}
+            status="idle"
+            createdAt="2024-10-13T12:34:56Z"
+            rate={{
+              value: 1600,
+              from: "USD",
+              to: "NGN",
+            }}
+            visualProofsOfPaymentUris={[]}
+          />
+
+          <Stack direction="row-reverse" spacing={2} sx={{ mt: 4, mb: 2 }}>
+            <Button
+              variant="contained"
+              fullWidth
+              color="primary"
+              onClick={() => {
+                handleAccept();
+                handleNext();
+              }}
+            >
+              Redeem
+            </Button>
+            <Button
+              variant="outlined"
+              fullWidth
+              color="error"
+              onClick={() => {
+                handleReject();
+                handleNext();
+              }}
+            >
+              Reject
+            </Button>
+          </Stack>
+        </>
+      );
+    },
+  },
+];
+
 export default function CryptoOrdersItemPage() {
-  return <div className="min-h-screen pt-8">Crypto Orders Details Page</div>;
+  return (
+    <>
+      <PageTitle text="Order Processing - Crypto" />
+      <div className="py-4 px-4">
+        <DynamicStepper
+          acceptanceSteps={[
+            ...initGeneralSteps,
+            {
+              label: "Verify Card Authenticity",
+              renderBody: (
+                { itemIndex, steps },
+                {
+                  handleBack,
+                  handleNext,
+                  handleAccept,
+                  handleReject,
+                }: StepItemHandlers
+              ) => {
+                return (
+                  <>
+                    <Stack direction="row" spacing={2} sx={{ mt: 4, mb: 2 }}>
+                      <Button
+                        variant="outlined"
+                        disabled={itemIndex === 0}
+                        onClick={handleBack}
+                      >
+                        Back
+                      </Button>
+
+                      <Button
+                        variant="contained"
+                        onClick={handleNext}
+                        disableElevation
+                      >
+                        {itemIndex === steps.length - 1 ? "Finish" : "Continue"}
+                      </Button>
+                    </Stack>
+                  </>
+                );
+              },
+            },
+            {
+              label: "Complete Payments",
+              description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+            },
+            {
+              label: "Complete Order",
+              description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+            },
+          ]}
+          rejectionSteps={[
+            ...initGeneralSteps,
+            {
+              label: "Create an ad group",
+              description:
+                "An ad group contains one or more ads which target a shared set of keywords.",
+            },
+            {
+              label: "Create an ad",
+              description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
 
   // Continue or Back+Next
   // I want to reject the order
@@ -8,9 +145,6 @@ export default function CryptoOrdersItemPage() {
 /*
 appealing, reviewing, processing, rejected
 */
-
-
-
 
 /*
 
@@ -33,9 +167,6 @@ on REJECTED
 
 
 */
-
-
-
 
 /*
 
