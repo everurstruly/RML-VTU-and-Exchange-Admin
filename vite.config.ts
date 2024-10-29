057330e913852f959b015c124f6cd72994d3ea54
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-29 06:49:41 -0700

    fix: recognize import alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react-swc'
 import generouted from '@generouted/react-router/plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from "path";
+import { defineConfig } from 'vite'
+import { ViteAliases } from 'vite-aliases'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     generouted(),
+    ViteAliases(),
     tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 })
