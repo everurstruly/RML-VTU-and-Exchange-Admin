6b355a1dec9531143ad10845ff48db942f95511d
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-12 16:09:27 -0800

    feat: improve visual aesthetics

--- src/pages/services/giftcard/card-action.tsx
+++ src/pages/services/giftcard/card-action.tsx
@@ -1,30 +1,40 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { ListItemIcon, ListItemText } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {
+  DeleteOutline,
+  DoDisturbOutlined,
+  EditOutlined,
+  VisibilityOffOutlined,
+} from "@mui/icons-material";
+import clsx from "clsx";
 
 const options = [
-  'None',
-  'Atria',
-  'Callisto',
-  'Dione',
-  'Ganymede',
-  'Hangouts Call',
-  'Luna',
-  'Oberon',
-  'Phobos',
-  'Pyxis',
-  'Sedna',
-  'Titania',
-  'Triton',
-  'Umbriel',
+  {
+    label: "Edit",
+    color: "action",
+    Icon: (props: any) => <EditOutlined {...props} />,
+  },
+  {
+    label: "Deactivate",
+    color: "action",
+    Icon: (props: any) => <DoDisturbOutlined {...props} />,
+  },
+  {
+    label: "Delete",
+    color: "error",
+    Icon: (props: any) => <DeleteOutline {...props} />,
+  },
 ];
 
 const ITEM_HEIGHT = 48;
 
-export default function LongMenu() {
+export default function GiftcardListItemActionButton() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -38,17 +48,18 @@ export default function LongMenu() {
       <IconButton
         aria-label="more"
         id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
         aria-haspopup="true"
         onClick={handleClick}
       >
         <MoreVertIcon />
       </IconButton>
+
       <Menu
         id="long-menu"
         MenuListProps={{
-          'aria-labelledby': 'long-button',
+          "aria-labelledby": "long-button",
         }}
         anchorEl={anchorEl}
         open={open}
@@ -57,14 +68,25 @@ export default function LongMenu() {
           paper: {
             style: {
               maxHeight: ITEM_HEIGHT * 4.5,
-              width: '20ch',
+              width: "24ch",
             },
           },
         }}
       >
-        {options.map((option) => (
-          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
-            {option}
+        {options.map(({ Icon, label, color }) => (
+          <MenuItem dense key={label} onClick={handleClose}>
+            <ListItemIcon>
+              <Icon fontSize="small" color={color} />
+            </ListItemIcon>
+            <span
+              className={clsx({
+                "text-primary": color === "primary",
+                "text-gray-500": color === "action",
+                "text-red-800": color === "error",
+              })}
+            >
+              {label}
+            </span>
           </MenuItem>
         ))}
       </Menu>
