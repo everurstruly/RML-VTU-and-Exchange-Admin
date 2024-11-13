c69bca0c1abdded7170783b1e42106132934cfa3
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-13 14:40:59 -0800

    feat: massively improve aesthetics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,8 @@ import {
   MoneyOutlined,
   StarBorderOutlined,
   StoreOutlined,
+  RepartitionOutlined,
+  BoltOutlined,
   TokenRounded,
 } from "@mui/icons-material";
 import {
@@ -41,42 +43,36 @@ export default function HomePage() {
         <h4 className="text-sm mb-2 font-medium">Statistics</h4>
 
         <div className="flex flex-wrap gap-4">
-          <article className="border rounded-lg p-2 grow gap-y-1.5 flex flex-col">
-            <h4 className="!text-xs text-zinc-500 font-medium">Profits</h4>
+          <article className="border rounded-lg p-2.5 grow gap-y-1.5 flex flex-col">
+            <h4 className="!text-xs text-zinc-500">Profits</h4>
 
             <div className="flex items-center gap-x-2">
               <div className="size-6 rounded-md border grid place-content-center">
                 <StoreOutlined className="!size-4 text-success" />
               </div>
-              <Typography variant="h6" className="text-zinc-800">
-                200
-              </Typography>
+              <Typography variant="subtitle1">200</Typography>
             </div>
           </article>
 
-          <article className="border rounded-lg p-2 grow gap-y-1.5 flex flex-col">
-            <h4 className="!text-xs text-zinc-500 font-medium">Activities</h4>
+          <article className="border rounded-lg p-2.5 grow gap-y-1.5 flex flex-col">
+            <h4 className="!text-xs text-zinc-500">Activities</h4>
 
             <div className="flex items-center gap-x-2">
               <div className="size-6 rounded-md border grid place-content-center">
                 <StarBorderOutlined className="!size-4 text-primary" />
               </div>
-              <Typography variant="h6" className="text-zinc-800">
-                200
-              </Typography>
+              <Typography variant="subtitle1">200</Typography>
             </div>
           </article>
 
-          <article className="border rounded-lg p-2 grow gap-y-1.5 flex flex-col">
-            <h4 className="!text-xs text-zinc-500 font-medium">Customers</h4>
+          <article className="border rounded-lg p-2.5 grow gap-y-1.5 flex flex-col">
+            <h4 className="!text-xs text-zinc-500">Customers</h4>
 
             <div className="flex items-center gap-x-2">
               <div className="size-6 rounded-md border grid place-content-center">
                 <FlagOutlined className="!size-4 text-error" />
               </div>
-              <Typography variant="h6" className="text-zinc-800">
-                200
-              </Typography>
+              <Typography variant="subtitle1">200</Typography>
             </div>
           </article>
         </div>
@@ -86,8 +82,8 @@ export default function HomePage() {
         <h4 className="text-sm mb-2 font-medium">Top-Up Service</h4>
 
         <Card variant="outlined" className="w-full p-4 !rounded-lg">
-          <label className="text-xs mb-1.5 text-zinc-400 inline-flex items-center gap-x-1">
-            <AccountBalanceWalletOutlined className="text-zinc-400 !size-4" />
+          <label className="text-xs mb-1.5 text-zinc-500 inline-flex items-center gap-x-1.5">
+            <AccountBalanceWalletOutlined className="text-zinc-500 !size-4" />
             Wallet Balance
           </label>
           <Typography className="text-zinc-700" variant="h5" fontWeight={500}>
@@ -101,20 +97,23 @@ export default function HomePage() {
             variant="outlined"
             className="!rounded-md !px-4 !py-1.5 w-full !justify-normal !border-zinc-300 !text-zinc-500"
             endIcon={<ArrowRightAlt />}
+            startIcon={<RepartitionOutlined />}
             classes={{ endIcon: "!ms-auto" }}
             size="small"
           >
             Restock Credits
           </Button>
+
           <Button
             color="inherit"
             variant="outlined"
             className="!rounded-md !px-4 !py-1.5 w-full !justify-normal !border-zinc-300 !text-zinc-500"
             endIcon={<ArrowRightAlt />}
             classes={{ endIcon: "!ms-auto" }}
+            startIcon={<BoltOutlined />}
             size="small"
           >
-            Update my retail prices
+            Update Retail Prices
           </Button>
         </Stack>
       </section>
@@ -123,23 +122,17 @@ export default function HomePage() {
         <h4 className="text-sm mb-2 font-medium">Exchange Services</h4>
         <Stack spacing={4}>
           <Stack spacing={1}>
-            <Card variant="outlined" sx={{ borderRadius: "4px" }}>
+            <Card variant="outlined" classes={{ root: "!rounded-lg" }}>
               <CardActionArea
-                sx={{ display: "flex" }}
+                sx={{ display: "flex", justifyContent: "space-between" }}
                 href="/services/giftcard"
               >
                 <CardContent>
-                  <Typography
-                    variant="h6"
-                    component="h4"
-                    mb={0.5}
-                    pt={0}
-                    className="text-zinc-700 pt-1.5 pb-.5"
-                  >
+                  <Typography variant="h6" pt={0} className="pt-1.5 pb-.5">
                     GIFT CARDS
                   </Typography>
 
-                  <Typography variant="body2" className="text-gray-500">
+                  <Typography variant="caption" className="text-gray-500">
                     Manage Your List of Cards and their Exchange Rates
                   </Typography>
                 </CardContent>
@@ -158,7 +151,7 @@ export default function HomePage() {
               size="small"
               startIcon={<AddCardOutlined />}
             >
-              Gift Card List and Rates
+              List and Rates
             </Button>
           </Stack>
 
