4436000c6fdf109ef4a9133e6dcdec0f53b59f7e
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-14 14:00:42 -0800

    refactor: add psuedo code

--- src/pages/services/giftcard/rates/new.tsx
+++ src/pages/services/giftcard/rates/new.tsx
@@ -3,9 +3,29 @@ import PageTitle from "@/components/page-title";
 export default function GiftcardRateCreationPage() {
   return (
     <main className="min-h-screen relative pb-32 pt-6">
-      <PageTitle text="Add New Gift Card Excange Rate" />
-      
-            
+      <PageTitle text="Giftcard Excange Rate - New" />
+
+      {/* <section>
+        <Image
+        <form action="#">
+
+        </form>
+      </section> */}
+
+      {/* [Image] [CardTitle] */}
+
+      {/* 1. Card Image
+      2. Card Title 
+      3.  */}
+      {/* When you receive:
+      Value From: 
+      Value Max To:
+      Region: []
+
+      You will pay
+      [Curr] [Amount] / [Curr]
+
+      // Region and Curr-to-or-per are of the same object which is Worth */}
     </main>
   );
 }
