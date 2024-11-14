63ae5921082f8e13eb70ad6d116b81391fc3aee9
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-14 14:00:15 -0800

    feat: reduce user action options

--- src/components/orders-table-filter-and-sort.tsx
+++ src/components/orders-table-filter-and-sort.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { IconButton, Stack } from "@mui/material";
 import OrderStatusSelectFilter from "./order-status-select-filter";
-import { FilterAlt } from "@mui/icons-material";
+import {
+  FilterAlt,
+  FilterAltOutlined,
+  FilterListOutlined,
+} from "@mui/icons-material";
 import {
   MRT_GlobalFilterTextField,
   MRT_ShowHideColumnsButton,
   MRT_ToggleDensePaddingButton,
+  MRT_ToggleGlobalFilterButton,
 } from "material-react-table";
 
 export default function OrdersTableFilterAndSort({ table }: { table: any }) {
   return (
     <Stack className="py-2 px-4" spacing={1.5}>
-      <div className="flex items-center gap-x-3">
+      <div className="flex items-end gap-x-3">
         <OrderStatusSelectFilter />
+
+        <div className="border rounded-lg p-1.5">
+          <MRT_ShowHideColumnsButton table={table} className="!p-0" />
+        </div>
 
         <IconButton
           sx={{
@@ -21,14 +30,14 @@ export default function OrdersTableFilterAndSort({ table }: { table: any }) {
             borderRadius: ".3875rem",
           }}
         >
-          <FilterAlt className="text-zinc-500" />
+          <FilterAltOutlined className="text-zinc-500" />
         </IconButton>
       </div>
 
+      <MRT_GlobalFilterTextField table={table} fullWidth />
       <div className="flex items-end gap-x-2">
         {/* FIX: make the wrapper element width 100% */}
-        <MRT_GlobalFilterTextField table={table}  />
-        
+
         {/* <div className="flex items-end gap-x-1.5">
           <TextField
             id="standard-basic"
@@ -47,13 +56,14 @@ export default function OrdersTableFilterAndSort({ table }: { table: any }) {
             fullWidth
           />
         </div> */}
+
         <div className="ms-auto flex items-center gap-x-3">
-          <div className="border rounded-lg p-1.5">
-            <MRT_ShowHideColumnsButton table={table} className="!p-0" />
-          </div>
-          <div className="border rounded-lg p-1.5">
+          {/* <div className="border rounded-lg p-1.5">
+            <MRT_ShowHideColumnsButton table={table} className="!p-0"  />
+          </div> */}
+          {/* <div className="border rounded-lg p-1.5">
             <MRT_ToggleDensePaddingButton table={table} className="!p-0" />
-          </div>
+          </div> */}
         </div>
       </div>
     </Stack>
