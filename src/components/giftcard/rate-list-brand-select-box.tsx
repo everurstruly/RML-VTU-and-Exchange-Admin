cf76ae3058560dd569ecb49cad26eb79a8897780
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-14 02:45:52 -0800

    feat: create new page contextual component

---
+++ src/components/giftcard/rate-list-brand-select-box.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { Theme, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = ["Amazon", "Steam", "Google", "Ebay", "Xbox", "Razor Gold"];
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function RateListBrandSelectBox() {
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  return (
+    <div className="border rounded-md px-4 py-2.5 flex items-center justify-between">
+      <h4 className="uppercase text-lg font-medium leading-none">Amazon</h4>
+      <div className="flex gap-x-6 items-center">
+        <Button variant="text" size="small" color="primary" disabled>
+          Compare
+        </Button>
+        <Button variant="text" size="small" color="success">
+          Change
+        </Button>
+      </div>
+    </div>
+  );
+}
