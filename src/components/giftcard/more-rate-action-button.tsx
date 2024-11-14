0c3830d6a8ab361f4c9185cfa44756be926855a7
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-14 12:54:54 -0800

    feat: remove unused flat style

--- src/components/giftcard/more-rate-action-button.tsx
+++ src/components/giftcard/more-rate-action-button.tsx
@@ -95,7 +95,6 @@ export default function GiftcardRateItemActionButton() {
                 {label}
               </span>
             </MenuItem>
-            {label !== "Delete" && <Divider className="!m-0" />}
           </div>
         ))}
       </Menu>
